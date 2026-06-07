9708e53c98c94158fd53f6d00328ab454d0191a1
Michael Seleman <michaelseleman@gmail.com>
2026-06-07 14:12:34 -0700

    release: 0.2.53

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
-let version = "0.2.52"
+let version = "0.2.53"
 let baseURL = "https://github.com/shortkit/shortkit-ios/releases/download/\(version)"
 
 let package = Package(
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "ShortKitSDK",
             url: "\(baseURL)/ShortKitSDK.xcframework.zip",
-            checksum: "a0fea9ca223d408ace374f4f6f1594ae5364a1bd9f84a53d655714196d489923"
+            checksum: "659700589857f06dd7041c372c49b6d8a24d174f8b16e00363bcee2cd4b0e571"
         ),
     ]
 )
